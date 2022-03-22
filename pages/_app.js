50ea47e3d7dc42429d8bc254d21ec8ea5c81d89e
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-03-22 21:27:55 +0900

    Changed routing for mobile and got rid of NavBar for mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import NavBar from "../components/NavBar";
 import "../styles/globals.css";
+import MediaQuery from "react-responsive";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <NavBar />
+      <MediaQuery minWidth={900}>
+        <NavBar />
+      </MediaQuery>
       <Component {...pageProps} />
     </div>
   );
